7aa3cc6485b2e37b7405d28a2560cdf29715a2af
Yorktsc <yooooork@berkeley.edu>
2019-08-07 17:49:25 +0800

    add a new method for longestConsecutive.py

--- longestConsecutive.py
+++ longestConsecutive.py
@@ -1,7 +1,26 @@
 class Solution:
     def longestConsecutive(self, nums):
-        longest_streak = 0
-        num_set = set(nums)
+        hash_dict = dict()
+        
+        max_length = 0
+        for num in nums:
+            if num not in hash_dict:
+                left = hash_dict.get(num - 1, 0)
+                right = hash_dict.get(num + 1, 0)
+                
+                cur_length = 1 + left + right
+                if cur_length > max_length:
+                    max_length = cur_length
+                
+                hash_dict[num] = cur_length
+                hash_dict[num - left] = cur_length
+                hash_dict[num + right] = cur_length
+                
+        return max_length
+
+	"""
+	longest_streak = 0
+        num_set = set(nums) #Changing a list into a hashset can speedup the time for access of elements
 
         for num in num_set:
             if num - 1 not in num_set:
@@ -15,7 +34,7 @@ class Solution:
                 longest_streak = max(longest_streak, current_streak)
 
         return longest_streak
-        
+        """
         
         """
         if not nums:
