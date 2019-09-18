c5afdda3e038f381ea742a97302774a9cd71970a
Yorktsc <yooooork@berkeley.edu>
2019-09-18 12:35:39 -0700

    add mergeSortedArray

---
+++ mergeSortedArray.py
@@ -0,0 +1,46 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        """
+        nums1_copy = nums1[:m] 
+        nums1[:] = []
+
+        # Two get pointers for nums1_copy and nums2.
+        p1 = 0 
+        p2 = 0
+        
+        # Compare elements from nums1_copy and nums2
+        # and add the smallest one into nums1.
+        while p1 < m and p2 < n: 
+            if nums1_copy[p1] < nums2[p2]: 
+                nums1.append(nums1_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+
+        # if there are still elements to add
+        if p1 < m: 
+            nums1[p1 + p2:] = nums1_copy[p1:]
+        if p2 < n:
+            nums1[p1 + p2:] = nums2[p2:]
+        """
+            
+            
+        # two get pointers for nums1 and nums2
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 = p2 - 1
+            else:
+                nums1[p] = nums1[p1]
+                p1 = p1 - 1
+            p = p - 1
+        if p2 >= 0:
+            nums1[:p2 + 1] = nums2[:p2 + 1]
