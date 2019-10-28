bb3e0e43c91c0457ed5bd3d7846a47260bea5a91
Yorktsc <yooooork@berkeley.edu>
2019-10-27 18:12:25 -0700

    add one more method to findKthLargest

--- findKthLargest.py
+++ findKthLargest.py
@@ -1,10 +1,21 @@
 import heapq
-#hel
 class Solution(object):
     def findKthLargest(self, nums, k):
+        ## N + K * Log N
+        """
         nums = [-num for num in nums]
         heapq.heapify(nums)
         res = float('inf')
         for _ in range(k):
             res = heapq.heappop(nums)
         return -res
+        """
+        
+        ## MinHeap N * Log K
+        h = nums[:k]
+        heapq.heapify(h)
+        for num in nums[k:]:
+            if num > h[0]:
+                heapq.heappop(h)
+                heapq.heappush(h, num)
+        return h[0]
